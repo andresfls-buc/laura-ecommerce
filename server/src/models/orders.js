d0dddf074b9537ee9185d36fd4eeda295f186605
Andres Landazabal <andresflsxx@gmail.com>
2026-02-17 13:02:27 +0900

    add: orders models

---
+++ server/src/models/orders.js
@@ -0,0 +1,111 @@
+import { Model, DataTypes } from "sequelize";
+import sequelize from "../config/sequelize.js";
+
+class Order extends Model {}
+
+Order.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+
+    customerName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },  
+
+    customerEmail: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+
+    customerPhone: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+
+    shippingAddress: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+
+    shippingCity: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+
+    shippingPostalCode: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+
+   
+
+    subtotal: {
+      type: DataTypes.DECIMAL(10, 2),
+      allowNull: false,
+      defaultValue: 0,
+    },
+    shippingCost: {
+      type: DataTypes.DECIMAL(10, 2),
+      allowNull: false,
+      defaultValue: 0,
+    },
+
+    totalAmount: {
+      type: DataTypes.DECIMAL(10, 2),
+      allowNull: false,
+      defaultValue: 0,
+    },
+
+     status: {
+      type: DataTypes.ENUM(
+        "pending", 
+        "paid",
+        "shipped",
+        "completed", 
+        "cancelled"
+    ),
+      defaultValue: "pending",
+    },
+
+    paymentMethod: {
+      type: DataTypes.ENUM(
+        "wompi", 
+        "nequi", 
+        "daviplata", 
+        "bank_transfer"
+      ),
+      allowNull: false,
+    },
+
+    transactionId: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+
+    paymentStatus: {
+      type: DataTypes.ENUM(
+        "unpaid", 
+        "paid", 
+        "refunded"
+    ),
+    },
+
+  },
+  {
+    sequelize,
+    modelName: "Order",
+    tableName: "orders",
+    timestamps: true,
+  }
+);
+
+export default Order;
